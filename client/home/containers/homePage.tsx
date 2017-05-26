9e497639e78aeff5a5c218ca143c38865ceb0309
abirkholz <andrew.a.birkholz@gmail.com>
2017-05-26 12:13:33 -0500

    Update config and versions
    # Conflicts:
    #	yarn.lock

--- client/home/containers/homePage.tsx
+++ client/home/containers/homePage.tsx
@@ -1,5 +1,5 @@
 import * as React from 'react';
-import * as CSSTransitionGroup from 'react-addons-css-transition-group';
+import {CSSTransitionGroup} from 'react-transition-group';
 import App from '../../app';
 import Home from '../';
 
